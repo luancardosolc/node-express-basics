f031cb1ed08a25ffbf016a05b4f72b7aba69c090
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-29 09:31:19 -0300

    Creating an api route to return a json array of objects

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const path = require('path');
 
 const app = express();
@@ -10,6 +11,37 @@ const app = express();
 // app.get('/sendFile', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
+
+const members = [
+  {
+    id: 1,
+    name: 'Luan Cardoso',
+    email: 'luan@luan.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Julian Pereira',
+    email: 'julian@julian.com',
+    status: 'inactive',
+  },
+  {
+    id: 3,
+    name: 'Pirlo Santos',
+    email: 'pirlo@pirlo.com',
+    status: 'inactive',
+  },
+  {
+    id: 4,
+    name: 'Lionel Oliveira',
+    email: 'lionel@lionel.com',
+    status: 'active',
+  },
+];
+
+app.get('/api/member', (req, res) => {
+  res.json(members);
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
