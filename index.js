6bc004d071ec801b8a7bce45568494a29d053f48
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-29 09:10:01 -0300

    Creating our first route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World</h1>');
+})
+
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
