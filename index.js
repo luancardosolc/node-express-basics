7ff6d39eb43247d954ef3fd9401ef582f414b738
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-29 10:05:13 -0300

    Getting current date with moment to the logger middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const path = require('path');
-const members = require('./Members');
+const express = require("express");
+const path = require("path");
+const moment = require("moment");
+const members = require("./Members");
 
 const app = express();
 
@@ -13,23 +14,28 @@ const app = express();
 // });
 
 const logger = (req, res, next) => {
-  console.log('logger middleware', `${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  next();
-}
+	console.log(
+		"logger middleware",
+		`${req.protocol}://${req.get("host")}${
+			req.originalUrl
+		}: ${moment().format()}`
+	);
+	next();
+};
 
 // Init middleware
 app.use(logger);
 
 // Gets all members
-app.get('/api/member', (req, res) => {
-  res.json(members);
+app.get("/api/member", (req, res) => {
+	res.json(members);
 });
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
