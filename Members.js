4daad884f5c4c2084913d5fde640d1b8083151aa
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-29 09:33:59 -0300

    Members model

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+  {
+    id: 1,
+    name: 'Luan Cardoso',
+    email: 'luan@luan.com',
+    status: 'active',
+  },
+  {
+    id: 2,
+    name: 'Julian Pereira',
+    email: 'julian@julian.com',
+    status: 'inactive',
+  },
+  {
+    id: 3,
+    name: 'Pirlo Santos',
+    email: 'pirlo@pirlo.com',
+    status: 'inactive',
+  },
+  {
+    id: 4,
+    name: 'Lionel Oliveira',
+    email: 'lionel@lionel.com',
+    status: 'active',
+  },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const res = require('express/lib/response');
 const path = require('path');
+const members = require('./Members');
 
 const app = express();
 
@@ -12,33 +12,7 @@ const app = express();
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
-const members = [
-  {
-    id: 1,
-    name: 'Luan Cardoso',
-    email: 'luan@luan.com',
-    status: 'active',
-  },
-  {
-    id: 2,
-    name: 'Julian Pereira',
-    email: 'julian@julian.com',
-    status: 'inactive',
-  },
-  {
-    id: 3,
-    name: 'Pirlo Santos',
-    email: 'pirlo@pirlo.com',
-    status: 'inactive',
-  },
-  {
-    id: 4,
-    name: 'Lionel Oliveira',
-    email: 'lionel@lionel.com',
-    status: 'active',
-  },
-];
-
+// Gets all members
 app.get('/api/member', (req, res) => {
   res.json(members);
 });
